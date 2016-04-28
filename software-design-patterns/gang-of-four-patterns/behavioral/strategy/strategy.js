e413f5051b6776929d86f49477021e0019977e19
Piotr Kowalski <piecioshka@gmail.com>
2016-04-28 08:45:55 +0200

    Add example for strategy pattern

--- software-design-patterns/gang-of-four-patterns/behavioral/strategy/strategy.js
+++ software-design-patterns/gang-of-four-patterns/behavioral/strategy/strategy.js
@@ -5,3 +5,48 @@
 // http://www.algorytm.org/wzorce-projektowe/strategia-strategy.html
 // http://burczu-programator.pl/blog/wzorce-projektowe-w-javascript-strategia
 
+class StrategyManager {
+    constructor() {
+        this._strategies = [];
+    }
+
+    addStrategy(name, handler) {
+        this._strategies.push({ name, handler })
+    }
+
+    getStrategy(mission) {
+        let strategyHandler = null;
+
+        this._strategies.forEach(function (strategy) {
+            if (strategy.name === mission.type) {
+                strategyHandler = strategy.handler;
+            }
+        });
+
+        return strategyHandler;
+    }
+}
+
+var strategy;
+var sm = new StrategyManager();
+
+// Add a lot of strategies.
+sm.addStrategy('device-a', () => console.log('A'));
+sm.addStrategy('device-b', () => console.log('B'));
+
+// Choose first strategy.
+strategy = sm.getStrategy({ type: 'device-a' });
+strategy();
+
+// Choose second strategy.
+strategy = sm.getStrategy({ type: 'device-b' });
+strategy();
+
+// Choose unsupported strategy.
+strategy = sm.getStrategy({ type: 'device-c' });
+
+try {
+    strategy();
+} catch (e) {
+    console.error(e);
+}
